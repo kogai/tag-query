777ad3c82692aae2e6f6792b99ea81ce8f3ae1e9
小飼慎一 <kogai0121@gmail.com>
2017-04-14 22:22:44 +0900

    add basic server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate dotenv;
-extern crate hyper;
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
 
 use dotenv::dotenv;
 use std::env;
@@ -12,6 +15,10 @@ fn main() {
         Err(_) => "3000".to_string(),
     };
     let client_id = env::var("INSTAGRAM_CLIENT_ID").expect("lack of instagram client id.");
+
+    Iron::new(|_: &mut Request| Ok(Response::with((status::Ok, "Hello world!"))))
+        .http(format!("localhost:{}", port))
+        .expect("Server start process is failed.");
 
     println!("{}-{}", port, client_id);
 }
