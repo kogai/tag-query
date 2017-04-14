e215cfcda6a8b351f890cd047635da5b867318d2
小飼慎一 <kogai0121@gmail.com>
2017-04-14 23:52:12 +0900

    Add templates

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 extern crate dotenv;
 extern crate iron;
+extern crate handlebars;
+extern crate handlebars_iron as hbs;
 #[macro_use]
 extern crate router;
+#[cfg(not(feature = "serde_type"))]
+extern crate rustc_serialize;
 
 use iron::prelude::*;
-use iron::status;
+use iron::modifiers::Redirect;
+use iron::{Url, status};
+use hbs::{Template, HandlebarsEngine, DirectorySource};
+use rustc_serialize::json::{Json};
 
 use dotenv::dotenv;
 use std::env;
+use std::collections::BTreeMap;
 
 static INSTAGRAM_OAUTH_URI: &'static str = "https://api.instagram.com/oauth/authorize/";
 static REDIRECT_URI: &'static str = "https://bento-photo.herokuapp.com/";
@@ -21,22 +29,38 @@ fn main() {
     };
 
     let client_id = env::var("INSTAGRAM_CLIENT_ID").expect("lack of instagram client id.");
-    println!("{}?client_id={}&redirect_uri={}&response_type=code",
-             INSTAGRAM_OAUTH_URI,
-             client_id,
-             REDIRECT_URI);
+    let client_secret = env::var("INSTAGRAM_CLIENT_SECRET").expect("lack of instagram client secret.");
+    let authorization_uri = format!("{}?client_id={}&redirect_uri={}&response_type=code",
+                                    INSTAGRAM_OAUTH_URI,
+                                    client_id,
+                                    REDIRECT_URI);
+    println!("{}", authorization_uri);
+    println!("{}", client_secret);
+
+    let mut hbse = HandlebarsEngine::new();
+    hbse.add(Box::new(DirectorySource::new("./templates/", ".hbs")));
+    if let Err(r) = hbse.reload() {
+        println!("{}", r);
+    }
 
     let router = router!(
         index: get "/" => |_: &mut Request| {
-            Ok(Response::with((status::Ok, "OK")))
+            let mut resp = Response::new();
+            let data = BTreeMap::<String, Json>::new();
+            resp.set_mut(Template::new("index", data)).set_mut(status::Ok);
+            Ok(resp)
         },
-        oauth: get "/:oauth" => |_: &mut Request| {
-            Ok(Response::with((status::Ok, "OAUTH OK")))
+        oauth: get "/:oauth" => move |_: &mut Request| {
+            Ok(Response::with((status::Found, Redirect(
+                Url::parse(authorization_uri.as_str()).expect(format!("authorization_uri is invalid => {}", authorization_uri).as_str())
+            ))))
         },
     );
 
-    println!("Server start on {}", port);
+    let mut chain = Chain::new(router);
+    chain.link_after(hbse);
 
-    Iron::new(router).http(format!("localhost:{}", port)).expect("Server start process is failed.");
+    println!("Server start on {}", port);
+    Iron::new(chain).http(format!("0.0.0.0:{}", port)).expect("Server start process is failed.");
 }
 
