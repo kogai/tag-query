8a52fc5f8edf73662d96958683d6819d75aaa32f
小飼慎一 <kogai0121@gmail.com>
2017-04-14 22:40:22 +0900

    add some static variables

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ use iron::status;
 use dotenv::dotenv;
 use std::env;
 
+static INSTAGRAM_OAUTH_URI: &str = "https://api.instagram.com/oauth/authorize/";
+static REDIRECT_URI: &str = "https://bento-photo.herokuapp.com/";
+
 fn main() {
     dotenv().ok();
 
@@ -14,12 +17,17 @@ fn main() {
         Ok(p) => p,
         Err(_) => "3000".to_string(),
     };
+
     let client_id = env::var("INSTAGRAM_CLIENT_ID").expect("lack of instagram client id.");
+    println!("{}?client_id={}&redirect_uri={}&response_type=code",
+             INSTAGRAM_OAUTH_URI,
+             client_id,
+             REDIRECT_URI);
+
+    println!("Server start on {}", port);
 
     Iron::new(|_: &mut Request| Ok(Response::with((status::Ok, "Hello world!"))))
         .http(format!("localhost:{}", port))
         .expect("Server start process is failed.");
-
-    println!("{}-{}", port, client_id);
 }
 
