0d7cb33821c0cb6a78817bc27d43c3e1ae1fd57c
小飼慎一 <kogai0121@gmail.com>
2017-04-14 22:09:32 +0900

    add hyper

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate dotenv;
+extern crate hyper;
 
 use dotenv::dotenv;
 use std::env;
@@ -8,7 +9,7 @@ fn main() {
 
     let port = match env::var("PORT") {
         Ok(p) => p,
-        Err(_) => "4000".to_string(),
+        Err(_) => "3000".to_string(),
     };
     let client_id = env::var("INSTAGRAM_CLIENT_ID").expect("lack of instagram client id.");
 
