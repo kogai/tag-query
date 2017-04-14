825283eb0d0d8415ea81e62a1f378540ef5d703e
小飼慎一 <kogai0121@gmail.com>
2017-04-14 22:05:13 +0900

    get environment variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+
 fn main() {
-    println!("Hello! world.");
+    dotenv().ok();
+
+    let port = match env::var("PORT") {
+        Ok(p) => p,
+        Err(_) => "4000".to_string(),
+    };
+    let client_id = env::var("INSTAGRAM_CLIENT_ID").expect("lack of instagram client id.");
+
+    println!("{}-{}", port, client_id);
 }
+
