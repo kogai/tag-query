e70192aea2e6e892c9f7dada031ca50a28f9c3ea
shinichi kogai <kogai0121@gmail.com>
2017-04-17 23:19:10 +0900

    replace to react

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,20 +1,66 @@
-const hashtag = document.querySelector("#hashtag");
-const username = document.querySelector("#username");
+const Search = (name, onChange) => React.createElement("div", {
+  className: 'uk-margin'
+}, [
+  React.createElement("label", {className: "uk-form-label"}, name),
+  React.createElement("div", {className: "uk-form-controls"}, React.createElement("input", {
+    className: "uk-input",
+    type: "text",
+    placeholder: `input ${name}`,
+    onChange: e => onChange(e.target.value),
+  })),
+]);
 
-hashtag.addEventListener("input", (e) => {
-  const value = e.target.value;
-  fetch(`/api/hashtag?user_id=31480262&hashtag=${value}`, {
-      credentials: 'include'
-    })
-    .then(r => r.json())
-    .then(r => console.log(r));
-});
+const Root = (state) => {
+  return React.createElement(
+    'div',
+    null,
+    [
+      Search("username", state.onChangeUsername),
+      state.user_ids.length > 0 && Search("hashtag", state.onChangeHashtag),
+      state.medias.map(m => React.createElement("img", {src: m.src}))
+    ]
+  );
+};
 
-username.addEventListener("input", (e) => {
-  const value = e.target.value;
-  fetch(`/api/username?username=${value}`, {
-      credentials: 'include'
-    })
-    .then(r => r.json())
-    .then(r => console.log(r));
-});
+class Container extends React.Component {
+  constructor() {
+    super();
+    const onChangeUsername = username => {
+      fetch(`/api/username?username=${username}`, {
+          credentials: 'include'
+        })
+        .then(r => r.json())
+        .then(r => r.data && this.setState({ user_ids: r.data.map(d => d.id) }));
+
+      this.setState({ username });
+    };
+    const onChangeHashtag = hashtag => {
+      const user_ids = this.state.user_ids;
+      fetch(`/api/hashtag?user_id=${user_ids[0]}&hashtag=${hashtag}`, {
+          credentials: 'include'
+        })
+        .then(r => r.json())
+        .then(r => r.data && this.setState({ medias: r.data.map(d => ({ src: d.images.thumbnail.url, link: d.link })) }));
+
+      this.setState({ hashtag });
+    };
+
+    this.state = {
+      username: "",
+      hashtag: "",
+      user_ids: [],
+      medias: [],
+      onChangeUsername,
+      onChangeHashtag,
+    };
+  }
+  render() {
+    console.log(this.state);
+    return Root(this.state);
+  }
+}
+
+ReactDOM.render(
+  React.createElement(Container),
+  document.getElementById("root")
+);
